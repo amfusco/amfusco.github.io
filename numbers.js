afa0465295034587b34d7e349897fd28574ee214
Alanna Mae Fusco <amf@c02nr1rzg083.local>
2019-01-30 22:08:36 -0500

    hw

---
+++ numbers.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html></html>
+    <head>
+     <meta charset="utf-8" />
+     <title> javascript test page</title>
+    </head>
+    <body>
+<script>
+
+function greaterThanTen(num) {
+
+if (num > 10) {
+  console.log("greater than 10!");
+}
+else {
+  console.log("less than 10!");
+}
+
+return num > 10;
+
+}
+
+</script>
+    </body>
+</html> 
